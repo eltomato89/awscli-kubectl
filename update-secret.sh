1bba97da63647fa0a00a54ecdcf10d8402408de9
Jens Köhler <eltomato@googlemail.com>
2019-10-29 15:29:33 +0100

    fixed base64 wrapping

--- update-secret.sh
+++ update-secret.sh
@@ -7,7 +7,7 @@ then
   login=$(echo "${BASH_REMATCH[1]}:${BASH_REMATCH[2]}" | base64)
   echo "Configuring registry ${BASH_REMATCH[4]:8}..."
   dockerconfig="{\"auths\":{\"${BASH_REMATCH[4]:8}\":{\"auth\": \"${login}\"}}}"
-  dockerconfigjson=$(echo ${dockerconfig} | base64)
+  dockerconfigjson=$(echo ${dockerconfig} | base64 -w0)
   secret="apiVersion: v1\nkind: Secret\nmetadata:\n  name: ${KUBE_SECRET_NAME}\ndata:\n  .dockerconfigjson: ${dockerconfigjson}\ntype: kubernetes.io/dockerconfigjson"
   echo -e ${secret} | kubectl replace -f - --force
   cat <<EOF
@@ -16,7 +16,7 @@ In order to use the new secret to pull images, add the following to your Pod def
 
     spec:
       imagePullSecrets:
-        - name: aws-ecr-credentials
+        - name: ${KUBE_SECRET_NAME}
       [...]
 
 Remember that AWS ECR login credentials expire in 12 hours!
